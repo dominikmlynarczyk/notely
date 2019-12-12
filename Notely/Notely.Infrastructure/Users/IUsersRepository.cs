84b577a12b7df1ee8c4df2170710be7c65f848cc
jakub.swiech@kotrak.com <jakub.swiech@kotrak.com>
2019-12-12 20:37:07 +0100

    add smth

---
+++ Notely/Notely.Infrastructure/Users/IUsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Notely.Domain.Users;
+
+namespace Notely.Infrastructure.Users
+{
+    public interface IUsersRepository
+    {
+        void Save(User user);
+    }
+}
---
+++ Notely/Notely.Infrastructure/Users/UserEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notely.Infrastructure.Users
+{
+    public class UserEntity
+    {
+    }
+}
