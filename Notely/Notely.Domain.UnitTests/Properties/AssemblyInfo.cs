19ef5df89886f54ed4f2daf11d4847e3b6929714
Sonor Sound <40466173+sonorsound@users.noreply.github.com>
2020-01-22 18:46:24 +0100

    Add unit tests for Users

---
+++ Notely/Notely.Domain.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Notely.Domain.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Notely.Domain.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("896fbc67-cd44-4cb0-9536-9143401c46ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Notely/Notely.Domain.UnitTests/UserTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Notely.Domain.Users;
+using Notely.Domain.Users.Policies;
+using Notely.SharedKernel;
+
+namespace Notely.Domain.UnitTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void Should_Login_User_With_Correct_Password()
+        {
+            var user = new User(new AggregateId(Guid.NewGuid()), "xxx", "xxx", "xxx", "xxx@xxx.com");
+
+            user.SetPassword("12345678", new FourLettersPasswordPolicy());
+
+            var result = user.IsPasswordValid("12345678");
+
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void Should_Not_Login_With_Incorrect_Password()
+        {
+            var user = new User(new AggregateId(Guid.NewGuid()), "xxx", "xxx", "xxx", "xxx@xxx.com");
+
+            user.SetPassword("12345678", new FourLettersPasswordPolicy());
+
+            var result = user.IsPasswordValid("123456");
+
+            Assert.IsFalse(result);
+        }
+    }
+}
